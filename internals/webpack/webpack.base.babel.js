b8e1df6876881fb620ca3e801b89cec702b806d9
CFLai1990 <music_lcf@163.com>
2019-02-11 17:10:28 +0800

    Testing speech-to-text. --ChufanLai

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -13,86 +13,87 @@ process.noDeprecation = true
 
 module.exports = options => ({
   entry: options.entry,
-  output: Object.assign(
-    {
+  output: Object.assign({
       // Compile into js/build.js
-      path: path.resolve(process.cwd(), 'build'),
-      publicPath: '/'
-    },
+    path: path.resolve(process.cwd(), 'build'),
+    publicPath: '/'
+  },
     options.output
   ), // Merge with env dependent settings
   module: {
-    rules: [
-      {
-        test: /\.js$/, // Transform all .js files required somewhere with Babel
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: options.babelQuery
-        }
-      },
-      {
+    rules: [{
+      test: /\.js$/, // Transform all .js files required somewhere with Babel
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: options.babelQuery
+      }
+    },
+    {
         // Preprocess our own .css files
         // This is the place to add your own loaders (e.g. sass/less etc.)
         // for a list of loaders, see https://webpack.js.org/loaders/#styling
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
+      test: /\.css$/,
+      exclude: /node_modules/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
         // Preprocess 3rd party .css files located in node_modules
-        test: /\.css$/,
-        include: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              progressive: true,
-              optimizationLevel: 7,
-              interlaced: false,
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              }
+      test: /\.css$/,
+      include: /node_modules/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      test: /\.(eot|svg|otf|ttf|woff|woff2)$/,
+      use: 'file-loader'
+    },
+    {
+      test: /\.(jpg|png|gif)$/,
+      use: [
+        'file-loader',
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            progressive: true,
+            optimizationLevel: 7,
+            interlaced: false,
+            pngquant: {
+              quality: '65-90',
+              speed: 4
             }
           }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: 'html-loader'
-      },
-      {
-        test: /\.json$/,
-        use: 'json-loader'
-      },
-      {
-        test: /\.(mp4|webm)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 10000
-          }
         }
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: 'csv-loader'
-      },
-      {
-        test: /\.xml$/,
-        use: ['xml-loader']
+      ]
+    },
+    {
+      test: /\.html$/,
+      use: 'html-loader'
+    },
+    {
+      test: /\.json$/,
+      use: 'json-loader'
+    },
+    {
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
       }
+    },
+    {
+      test: /\.(csv|tsv)$/,
+      use: 'csv-loader'
+    },
+    {
+      test: /\.xml$/,
+      use: ['xml-loader']
+    }
     ]
+  },
+  node: {
+    fs: 'empty'
   },
   plugins: options.plugins.concat([
     new webpack.ProvidePlugin({
